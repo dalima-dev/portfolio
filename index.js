f23d7c50a1c8ae05e3ae9af62e5326b79a693b4f
Daniel Alves de Lima <dalima.dev@gmail.com>
2022-04-03 11:39:43 -0300

    update

--- index.js
+++ index.js
@@ -7,8 +7,36 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = 3000;
 
+let language = "portuguese";
+
+const projects = [
+  {
+    title: "titulo",
+    src: "",
+  },
+];
+const stacks = [
+  {
+    name: "HTML",
+    src: "/icons/html-5.png",
+  },
+  {
+    name: "CSS",
+    src: "/icons/css-3.png",
+  },
+  {
+    name: "JavaScript",
+    src: "/icons/js.png",
+  },
+
+  {
+    name: "Express",
+    src: "/icons/express.png",
+  },
+];
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { stacks, projects });
 });
 
 app.listen(PORT, () =>
