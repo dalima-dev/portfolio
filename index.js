75fe8e4080ae5f72f624d266526091190aa7addc
Daniel Alves de Lima <dalima.dev@gmail.com>
2022-04-05 17:44:02 -0300

    ready

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const projects = [
   {
