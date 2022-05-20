8b125239be0641198c04a8eb4e9a1acc3f2c19a5
Daniel Alves <dalima.dev@gmail.com>
2022-05-20 07:51:03 -0300

    stacks

--- index.js
+++ index.js
@@ -61,21 +61,15 @@ const stacks = [
   },
   */
   {
-    id: "nest",
-    name: "Nest",
-    image_src:
-      "https://d33wubrfki0l68.cloudfront.net/e937e774cbbe23635999615ad5d7732decad182a/26072/logo-small.ede75a6b.svg",
-  },
-  {
     id: "react",
     name: "React",
     image_src: "/icons/react.png",
   },
   {
-    id: "tailwindcss",
-    name: "Tailwind CSS",
+    id: "nest",
+    name: "Nest",
     image_src:
-      "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+      "https://d33wubrfki0l68.cloudfront.net/e937e774cbbe23635999615ad5d7732decad182a/26072/logo-small.ede75a6b.svg",
   },
 ];
 
